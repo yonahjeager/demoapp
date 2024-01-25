b32028bf338bbd1bf9e966a4b9bfa81203a58de7
Jonah Yeager <jonahyeager@Jonahs-MBP.home>
2024-01-25 08:27:28 -0700

    Created a main controller with home about contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'main#home'
+  get '/home', to: 'main#home'
+  get '/about', to: 'main#about'
+  get '/contact', to: 'main#contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
